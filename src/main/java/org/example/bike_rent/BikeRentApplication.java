fb4cba9cd5e3467421a9556b0227042fc684b9a9
Adam <adam.maka.tp@gmail.com>
2025-03-20 18:05:09 +0100

    Initial commit

---
+++ src/main/java/org/example/bike_rent/BikeRentApplication.java
@@ -0,0 +1,13 @@
+package org.example.bike_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bike_rent/BikeRentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bike_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
