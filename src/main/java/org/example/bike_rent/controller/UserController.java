2d16a5705bf5d0b9d29c3fc223578d485f46ef32
Adam <adam.maka.tp@gmail.com>
2025-03-22 17:52:51 +0100

    Create user controller with basic endpoints

---
+++ src/main/java/org/example/bike_rent/controller/UserController.java
@@ -0,0 +1,35 @@
+package org.example.bike_rent.controller;
+
+import java.util.List;
+import org.example.bike_rent.entity.User;
+import org.example.bike_rent.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+}
