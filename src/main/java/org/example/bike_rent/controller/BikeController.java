cc965f1037135933e689fb94c3e59c1927eb536f
Adam <adam.maka.tp@gmail.com>
2025-05-27 19:05:33 +0200

    Add update endpoint for bikes with admin authorization

--- src/main/java/org/example/bike_rent/controller/BikeController.java
+++ src/main/java/org/example/bike_rent/controller/BikeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,9 +44,28 @@ public class BikeController {
     }
 
     @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<Bike> updateBike(@PathVariable Long id, @RequestBody Bike bikeDetails) {
+        Optional<Bike> optionalBike = bikeService.getBikeById(id);
+        if (optionalBike.isPresent()) {
+            Bike bike = optionalBike.get();
+            bike.setModel(bikeDetails.getModel());
+            bike.setBrand(bikeDetails.getBrand());
+            bike.setColor(bikeDetails.getColor());
+            bike.setAvailable(bikeDetails.isAvailable());
+            bike.setSerialNumber(bikeDetails.getSerialNumber());
+            Bike updatedBike = bikeService.saveBike(bike);
+            return ResponseEntity.ok(updatedBike);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteBike(@PathVariable Long id) {
         bikeService.deleteBike(id);
         return ResponseEntity.ok().build();
     }
 }
+
