db4c192f2b43f01236372717e1607304517133f8
Adam <adam.maka.tp@gmail.com>
2025-05-28 17:14:47 +0200

    Add phoneNumber and isActive fields to Customer entity

--- src/main/java/org/example/bike_rent/entity/user/Customer.java
+++ src/main/java/org/example/bike_rent/entity/user/Customer.java
@@ -2,12 +2,18 @@ package org.example.bike_rent.entity.user;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @DiscriminatorValue("CUSTOMER")
 public class Customer extends User {
     public Customer() {
         super();
     }
-}
 
+    private String phoneNumber;
+    private boolean isActive;
+}
