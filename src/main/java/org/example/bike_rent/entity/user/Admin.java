47d2684af8a67e435709343bdb1cefcae4680ea8
Adam <adam.maka.tp@gmail.com>
2025-05-28 17:19:43 +0200

    Add workerCode field to Admin entity and apply Lombok annotations

--- src/main/java/org/example/bike_rent/entity/user/Admin.java
+++ src/main/java/org/example/bike_rent/entity/user/Admin.java
@@ -2,12 +2,17 @@ package org.example.bike_rent.entity.user;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @DiscriminatorValue("ADMIN")
 public class Admin extends User {
+    private String workerCode;
+
     public Admin() {
         super();
     }
 }
-
